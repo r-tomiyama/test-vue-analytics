026f19ee08c6ad541350e7a108994916abbf1c32
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-01-14 01:04:27 +0900

    feat: google analytics イベントトラッキング

--- src/components/HelloPoyo.vue
+++ src/components/HelloPoyo.vue
@@ -1,6 +1,10 @@
 <template>
   <div style="display:flex; justify-content: center;">
-    <button type="button" @click="poyo">
+    <button
+      type="button"
+      @click="poyo"
+      :v-ga="$ga.event('click', 'bark', 'poyo')"
+    >
       ぽよ
     </button>
   </div>

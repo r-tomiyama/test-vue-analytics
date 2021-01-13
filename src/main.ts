74051e8e02e5493b2a1a15d468bd4dd19d0c1b2a
r-tomiyama <r.tomiyama@opt.ne.jp>
2021-01-14 00:31:47 +0900

    feat: google analytics でページビュー計測

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueAnalytics from "vue-analytics";
 import VueCompositionApi from "@vue/composition-api";
 import App from "./App.vue";
 import router from "./router";
@@ -6,6 +7,10 @@ import router from "./router";
 Vue.config.productionTip = false;
 
 Vue.use(VueCompositionApi);
+Vue.use(VueAnalytics, {
+  id: "",
+  router
+});
 
 new Vue({
   router,
